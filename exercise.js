eee877cda26e67f89313e0c8c444d19c86e3395a
tomasysh <tomasysh@gmail.com>
2025-06-19 13:45:54 +0800

    醒醒腦：js api some & every

---
+++ exercise.js
@@ -0,0 +1,12 @@
+
+
+const students =[{
+    name: 'Tomas',
+    gender: 'male',
+}];
+
+// TODO: 這個班級是否有男生
+students.some((student) => student.gender === 'male');
+
+// TODO: 這班級是否全部是男生
+students.every((student) => student.gender === 'male');
